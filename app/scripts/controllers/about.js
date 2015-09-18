405e59d1a2e8b0467a9af7b0efd8cd3106925efa
unknown <mbaranowski@EXO-MBARANOWSKI.wan.exorigo.pl>
2015-09-18 16:33:18 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cvApp
+ */
+angular.module('cvApp')
+    .controller('AboutCtrl', function ($scope) {
+
+    });
